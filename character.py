b2a6e39bda382f8ddf9180f1f2d74c249a3f6100
Michael Barnabi <rbarnabi676@gmail.com>
2017-10-15 15:42:48 -0500

    Added additional data points and key finding info

--- character.py
+++ character.py
@@ -6,6 +6,11 @@ import sys
 char_info_call = requests.get('https://api.xivdb.com/character/' + str(sys.argv[1]))
 char_info = char_info_call.json()
 
-#char_json = char_info.json()
 print(char_info.keys())
-print(char_info('name'))
+print(str('Name: ') + char_info['name'])
+print(str('Server: ') + char_info['server'])
+print(str('Last Active: ') + char_info['last_active'])
+
+char_data = char_info['data']
+
+print(char_data.keys())
