6566a65fb2e8f50451b934475ce4a55f4e4ca2eb
Michael Barnabi <rbarnabi676@gmail.com>
2017-10-15 19:15:45 -0500

    Added Mainhand
    Now returns mainhand item name and ilvl, and avg ilvl

--- character.py
+++ character.py
@@ -6,11 +6,22 @@ import sys
 char_info_call = requests.get('https://api.xivdb.com/character/' + str(sys.argv[1]))
 char_info = char_info_call.json()
 
-print(char_info.keys())
+#print(char_info.keys())
 print(str('Name: ') + char_info['name'])
 print(str('Server: ') + char_info['server'])
 print(str('Last Active: ') + char_info['last_active'])
 
 char_data = char_info['data']
 
-print(char_data.keys())
+#print(char_data.keys())
+char_gearsets_request = requests.get('https://api.xivdb.com/character/' + str(sys.argv[1]) + str('?data=gearsets'))
+
+char_gearsets = char_gearsets_request.json()
+gearset_dic = char_gearsets[0]
+print(str('Item Level Avg: ') + str(gearset_dic['item_level_avg']))
+gear_gear = (gearset_dic['gear'])
+
+mainhand = (gear_gear['slot_mainhand']['data'])
+#print(mainhand.keys())
+print(str('Main Hand: ') + mainhand['name'] + str(', ') + str(mainhand['level_item']))
+#print(mainhand['level_item'])
